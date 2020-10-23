1427dbcb92448d5444354d6076a164aee0d64721
Sarah <sarah.tiefert@htwg-konstanz.de>
2020-10-23 13:09:35 +0200

    created TUI, made game work with endrecursive function

--- src/main/scala/Connect4.scala
+++ src/main/scala/Connect4.scala
@@ -3,61 +3,23 @@ import model.{Grid, Player}
 import scala.io.StdIn
 import java.util.Scanner
 
+import controller.Controller
+
 import scala.util.control.Breaks.break
 import view.TUI
 
 object Connect4 {
   def main(args: Array[String]): Unit = {
-    val test = new Grid
-    val scanner = new Scanner(System.in)
+    val controller = new Controller
+    val tui = new TUI()
 
     // note: statt schleife eine endrekursion aufrufen. That would be proper functional programming!
-    var input = "0"
 
-   def move(player:Int): Unit ={
-     if (test.checkConnect4(test.put(input.toInt, player),player)) {
-       println(test.toString)
-       println("Player " + player + " hat gewonnen!")
-       input = "q"
-     }
-   }
+    def processInput(input: String): Unit ={
 
-    var players = 1::2::Nil
+    }
 
-    def processInput(){}
+    tui.startGame()
 
-    do {
-      println(test.toString)
-      println("player " + players.head + " input column (between 0 and 6)")
-      input = scanner.next()
-      if (input != "q") move(players.head)
-      players = players.tail:::players.head::Nil
-
-    } while(input != "q")
-
-    /*
-    test.put(1,2)
-    test.put(2,1)
-    test.put(2,2)
-    test.put(3,1)
-    test.put(3,1)
-    test.put(3,2)
-    test.put(4,1)
-    test.put(4,1)
-    test.put(4,1)
-    test.put(4,2)
-    test.put(2,1)
-    test.put(1,1)
-    test.put(1,1)
-    test.put(1,1)
-    //test.put(0,1)
-    //test.put(2,1)
-    //test.put(3,1)
-    //test.put(1,2)
-    //test.put(1,2)
-    //test.put(1,2)
-    println(test.toString)
-    println(test.checkConnect4((0,6),2))
-    */
   }
 }
--- src/main/scala/view/TUI.scala
+++ src/main/scala/view/TUI.scala
@@ -1,11 +1,35 @@
 package view
 
-abstract class TUI {
+import java.lang.ModuleLayer.Controller
+import java.util.Scanner
 
-  //
-  def processInput(input:String)
+import model.Grid
+import utils.Observer
 
-  def showBoard(): Unit ={
-    println()
+class TUI(){
+
+  //controller.add(this)
+
+  def startGame(): Unit ={
+    println("Willkommen zu Vier gewinnt!")
+
+    val grid = new Grid
+    val players = 1::2::Nil
+    val scanner = new Scanner(System.in)
+
+    move(grid, players, scanner)
+
+
+  }
+
+  def move(grid: Grid, players:List[Int], scanner:Scanner): Unit ={
+    println(grid.toString)
+    println("player " + players.head + " input column (between 0 and 6)")
+    if (grid.checkConnect4(grid.put(scanner.nextInt(), players.head),players.head)) {
+      println(grid.toString)
+      println("Player " + players.head + " hat gewonnen!")
+      return
+    }
+    move(grid, players.tail:::players.head::Nil, scanner)
   }
 }
